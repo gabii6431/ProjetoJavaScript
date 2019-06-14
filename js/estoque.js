b78df8b1071822902d61c8843e43f0d5e76dadbf
gabii6431 <gabii6431@gmail.com>
2019-06-14 10:25:18 -0300

    Revert "Cadastra produto e Lista produto javaScript"
    This reverts commit 6743f531d87c43e2293219de7e2d1b3e372e18b2.

--- js/estoque.js
+++ js/estoque.js
@@ -1,41 +1,39 @@
 
-var id;
-var indice_selecionado;
-var listaProdutos;
 if(sessionStorage.getItem('primeiraExecucao') == null){
-    sessionStorage.setItem('primeiraExecucao', 'false');
-    id = 0;
-    indice_selecionado = -1;
-    listaProdutos = sessionStorage.getItem("listaProdutos");
+    sessionStorage.setItem('primeiraExecucao', false);
+    var id = 0;
+    var indice_selecionado = -1;
+    var listaProdutos = [];
     listaProdutos = JSON.parse(listaProdutos);
     if(listaProdutos == null) {
         listaProdutos = [];
     }
-    sessionStorage.setItem("idProduto", id);
-    sessionStorage.setItem("indice_selecionado", indice_selecionado);
-    sessionStorage.setItem("listaProdutos", JSON.stringify(listaProdutos));
-
 }
 
 
 function Adicionar(){
-    var idProduto = parseInt(sessionStorage.getItem("idProduto"))+ 1 ;
+    var produto = [];
+    produto.push(document.getElementById('id').value);
+    produto.push(document.getElementById('nomeProduto').value);
+    produto.push(document.getElementById('dataValidadeProduto').value);
+    produto.push(document.getElementById('taxaImpostoProduto').value);  
+    produto.push(document.getElementById('quantidade').value);
+    produto.push(document.getElementById('preco').value);
+    produto.push(document.getElementById('funcionarioProduto').value);
 
-    var produto = JSON.stringify({
-        Id : idProduto,
-        Nome : document.getElementById('nomeProduto').value,
-        DataDeValidade : document.getElementById('dataValidadeProduto').value,
-        TaxaImposto : document.getElementById('taxaImpostoProduto').value,
-        Quantidade : document.getElementById('quantidade').value,
-        Preco :  document.getElementById('preco').value,
-        FuncionarioRes : document.getElementById('funcionarioProduto').value
-    });
-    sessionStorage.setItem("idProduto", idProduto);
-    let lista = sessionStorage.getItem("listaProdutos");
-    let listaProdutos =JSON.parse(lista);
-
-    listaProdutos.push(produto);
-    sessionStorage.setItem("listaProdutos", JSON.stringify(listaProdutos));
+    
+    // Id : document.getElementById('id').value,
+        // Nome : document.getElementById('nomeProduto').value,
+        // DataDeValidade : document.getElementById('dataValidadeProduto').value,
+        // TaxaImposto : document.getElementById('taxaImpostoProduto').value,
+        // Quantidade : document.getElementById('quantidade').value,
+        // Preco :  document.getElementById('preco').value,
+        // FuncionarioRes : document.getElementById('funcionarioProduto').value
+        
+    console.log(produto);
+    
+    //listaProdutos.push(produto);
+    //sessionStorage.setItem("listaProdutos", JSON.stringify(listaProdutos));
     alert("Registro adicionado.");
     return true;
 }
@@ -50,11 +48,6 @@ function Editar(){
         FuncionarioRes : document.getElementById('funcionarioProduto').value
     });
     sessionStorage.setItem("listaProdutos", JSON.stringify(listaProdutos));
-    let lista = sessionStorage.getItem("listaProdutos");
-    let listaProdutos =JSON.parse(lista);
-
-    listaProdutos.push(produto);
-    sessionStorage.setItem("listaProdutos", JSON.stringify(listaProdutos));
     alert("Informações editadas.")
     return true;
 }
@@ -66,42 +59,106 @@ function Excluir(){
 }
 
 function Listar(){
-    let listaProdutos = sessionStorage.getItem("listaProdutos");
-    let lista =JSON.parse(listaProdutos);
-    
-    console.log("Lista: "+lista);
-    $("#tabelaProduto").html("");
-    $("#tabelaProduto").html(
-        "<thead class='thead-dark'>"+
+    $(".tabelaProduto").html("");
+    $(".tabelaProduto").html(
+        "<thead>"+
         "   <tr>"+
-        "   <th style='text-align:center'>Id</th>"+
-        "   <th style='text-align:center'>Nome</th>"+
-        "   <th style='text-align:center'>Data de validade</th>"+
-        "   <th style='text-align:center'>Taxa de Imposto</th>"+
-        "   <th style='text-align:center'>Quantidade</th>"+
-        "   <th style='text-align:center'>Preço</th>"+
-        "   <th style='text-align:center'>Funcionario</th>"+
-        "   <th style='text-align:center'></th>"+
-        "   <th style='text-align:center'></th>"+
-        "   </tr >"+
+        "   <th>Id</th>"+
+        "   <th>Nome</th>"+
+        "   <th>Data de validade</th>"+
+        "   <th>Taxa de Imposto</th>"+
+        "   <th>Quantidade</th>"+
+        "   <th>Preço</th>"+
+        "   <th>Funcionario</th>"+
+        "   <th></th>"+
+        "   <th></th>"+
+        "   </tr>"+
         "</thead>"+
         "<tbody>"+
         "</tbody>"
-        )
-    for(var i in lista){
-        var produto = JSON.parse(lista[i]);
-        console.log(produto);
-        $("#tabelaProduto tbody").append("<tr>");
-        $("#tabelaProduto tbody").append("<td style='text-align:center'>"+produto.Id+"</td>");
-        $("#tabelaProduto tbody").append("<td style='text-align:center'>"+produto.Nome+"</td>");
-        $("#tabelaProduto tbody").append("<td style='text-align:center'>"+produto.DataDeValidade+"</td>");
-        $("#tabelaProduto tbody").append("<td style='text-align:center'>"+produto.TaxaImposto+"</td>");
-        $("#tabelaProduto tbody").append("<td style='text-align:center'>"+produto.Quantidade+"</td>");
-        $("#tabelaProduto tbody").append("<td style='text-align:center'>"+produto.Preco+"</td>");
-        $("#tabelaProduto tbody").append("<td style='text-align:center'>"+produto.FuncionarioRes+"</td>");
-        $("#tabelaProduto tbody").append("<td style='text-align:center'><a href='editar-produto.html'><img src='img/editar-icone.png' alt='"+i+"'  class = 'btnEditar'></a></td>");
-        $("#tabelaProduto tbody").append("<td style='text-align:center'><a href=''><img src='img/deletar-icone.png' alt='"+i+"' class = 'btnExcluir'></a></td>");
-        $("#tabelaProduto tbody").append("</tr>");
+        );
+    for(var i in listaProdutos){
+        var produto = JSON.parse(listaProdutos[i]);
+        $(".tabelaProduto tbody").append("<tr>");
+        $(".tabelaProduto tbody").append("<td>"+produto.Id+"</td>");
+        $(".tabelaProduto tbody").append("<td>"+produto.Nome+"</td>");
+        $(".tabelaProduto tbody").append("<td>"+produto.DataDeValidade+"</td>");
+        $(".tabelaProduto tbody").append("<td>"+produto.TaxaImposto+"</td>");
+        $(".tabelaProduto tbody").append("<td>"+produto.Quantidade+"</td>");
+        $(".tabelaProduto tbody").append("<td>"+produto.FuncionarioRes+"</td>");
+        $(".tabelaProduto tbody").append("<td><a href='editar-produto.html'><img src='img/editar.png' alt='"+i+"'  class = 'btnEditar'></a></td>");
+        $(".tabelaProduto tbody").append("<td><a href='editar-produto.html'><img src='img/editar.png' alt='"+i+"' class = 'btnExcluir'></a></td>");
+        $(".tabelaProduto tbody").append("</tr>");
+        
     }
 }
+ 
+$('#salvarProduto').on('click',function(){
+   console.log("Entrou")
+    Adicionar();
+    Listar();
+});
+ 
+$(".tabelaProduto").on("click", ".btnEditar", function(){
 
+    indice_selecionado = parseInt($(this).attr("alt"));
+    var produto = JSON.parse(listaProdutos[indice_selecionado]);
+    //editar nome classes 
+    $("#txtCodigo").val(produto.Nome);
+    $("#txtNome").val(produto.DataDeValidade);
+    $("#txtTelefone").val(produto.TaxaImposto);
+    $("#txtEmail").val(produto.Quantidade);
+    $("#txtEmail").val(produto.funcionarioRes);
+    $("#txtCodigo").attr("readonly","readonly");
+    $("#txtNome").focus();
+});
+
+$(".formularioEditar").on("submit",function(){
+    Editar();
+    Listar();
+});
+ 
+$(".tabelaProduto").on("click", ".btnExcluir",function(){
+    indice_selecionado = parseInt($(this).attr("alt"));
+    Excluir();
+    Listar();
+});
+
+
+
+
+
+// var formProduto = $('.formulario-produto');
+// var produtoNovo;
+// sessionStorage.setItem("idProduto", 0);
+
+
+// formProduto.submit(function (e) {
+
+//     e.preventDefault();
+//     var nomeProduto = document.getElementById('nomeProduto').value;
+//     var dataDeValidade = document.getElementById('dataValidadeProduto').value;
+//     var taxaImposto = document.getElementById('taxaImpostoProduto').value;
+//     var quantidade = document.getElementById('quantidade').value;
+//     var preco = document.getElementById('preco').value;
+//     var funcionarioRes = document.getElementById('funcionarioProduto').value;
+//     sessionStorage.setItem('idProduto', (sessionStorage.getItem() +1)).value;
+
+//     var lista_produtos = JSON.parse(localStorage.getItem('lista-produtos') || '[]');
+//     lista_produtos.push({
+//         id = sessionStorage.getItem(),
+//         nomeProduto = nomeProduto,
+//         dataDeValidade = dataDeValidade,
+//         taxaImposto = taxaImposto,
+//         quantidade = quantidade,
+//         preco = preco,
+//         funcionarioRes = funcionarioRes
+//     });
+//     localStorage.setItem("lista-pessoas", JSON.stringify(lista_produtos));
+
+//     console.log('Salva com sucesso.');
+// });
+
+// function Listar(){
+//     $("#tblLiWistar tbody").append("</tr>");
+// }
